1a5df582093816feabcbb6f9ea35d8acf2009583
ekunoff <ekunoff@factory42.com>
2016-10-20 10:16:14 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define("CLIENT_ID", "3MVG98_Psg5cppyarRODi_XbVz5pg617IH1lGUSXXVvgSAlTqmhG_ThqMDMUyqYAb4OHTsJ4Uyh15DKxQOzqb
+");
+define("CLIENT_SECRET", "5237947245643914754");
+define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+define("LOGIN_URI", "https://login.salesforce.com");
+?>
